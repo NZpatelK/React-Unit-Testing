c1e7bb9d69d073a7997dd1f374a861369af999a7
NZpatelK <patelk.dev@hotmail.com>
2023-01-08 14:17:38 +1300

    Add comment for button testing

--- learning-unit-testing/src/ButtonTesting/ButtonTesting.js
+++ learning-unit-testing/src/ButtonTesting/ButtonTesting.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+/*
+ButtonTesting is to test the button to check if the button is disbaled or not.
+prop variable has one prop is disabled value. Disabled value is boolean value 
+and that value is to set button to be disabled or not. 
+*/
 function ButtonTesting(prop) {
   return (
     <div>
